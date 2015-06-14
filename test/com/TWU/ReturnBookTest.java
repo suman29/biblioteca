da6ac3fa8231060cdf97fbc0e5bc7ef28fe32bc1
sumankum <sumankum@INsumankum.local>
2015-06-14 16:59:35 +0530

    Added spec to return book in ReturnBook class

--- test/com/TWU/ReturnBookTest.java
+++ test/com/TWU/ReturnBookTest.java
@@ -1,12 +1,29 @@
 package com.twu;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ReturnBookTest {
-    @Test
-    public  void shouldAllowUserToReturnBook() {
-//        ReturnBook returnBook = new ReturnBook();
+    @Mock
+    Library library;
 
+    @Mock
+    ReturnBookView returnBookView;
+
+    @Test
+    public void shouldBeAbleToReturnBook() {
+        when(returnBookView.takeUserInputForReturningBook())
+                .thenReturn(1);
+        ReturnBook returnBook = new ReturnBook(library, returnBookView);
+
+        returnBook.perform();
+        verify(library).returnBook(1);
     }
 
 }
