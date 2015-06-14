9026cff901a5c9a2c187db5e769a5bc361e91822
sumankum <sumankum@INsumankum.local>
2015-06-14 16:41:26 +0530

    Added spec for displaying the list of checkedout bboks in ReturnBookView class

--- src/com/TWU/ReturnBookView.java
+++ src/com/TWU/ReturnBookView.java
@@ -23,4 +23,8 @@ public class ReturnBookView {
             return -1;
         }
     }
+
+    public void displayListOfBooks() {
+        booksView.displayListOfBooks();
+    }
 }
--- test/com/TWU/ReturnBookViewTest.java
+++ test/com/TWU/ReturnBookViewTest.java
@@ -1,18 +1,26 @@
 package com.twu;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ReturnBookViewTest {
+
+    private ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() throws Exception {
+        System.setOut(new PrintStream(outputStream));
+    }
 
     @Mock
     BooksView booksView;
@@ -29,6 +37,20 @@ public class ReturnBookViewTest {
         int expectedInput = 1;
 
         assertEquals(expectedInput, actualInput);
+    }
+
+    @Test
+    public void shouldBeAbleToDisplayList() {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        ReturnBookView returnBookView = new ReturnBookView(bufferedReader,booksView);
+        returnBookView.displayListOfBooks();
+
+        Mockito.verify(booksView).displayListOfBooks();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.setOut(null);
     }
 
 }
